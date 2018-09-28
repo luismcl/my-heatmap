a188eee8ad1c9dcedc394e176425fc2e8aa9b7a8
lcisneros <lcisneros@datiobd.com>
2018-09-28 12:28:53 +0200

    Not Remember...

--- src/main/scala-2.10/BuildPoints.scala
+++ src/main/scala-2.10/BuildPoints.scala
@@ -10,12 +10,20 @@ object BuildPoints {
   def main(args: Array[String]) {
 
     val conf = new SparkConf().setAppName("Simple Application")
+    conf.setMaster("local[4]")
     val sc = new SparkContext(conf)
 
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     val df = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "trkpt").load("activities/*.gpx")
 
-    val rows = df.select(df("@lat"),df("@lon")).map(row => ((round(row.getDouble(0)) ,round(row.getDouble(1))),1)).reduceByKey((a, b) => a + b)
+
+    import org.apache.spark.sql.functions.udf
+    def udfRound=udf((value: Double) => {
+      BigDecimal(value).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble
+      })
+
+    val roundedDF = df.withColumn("redLat", udfRound(df("@lat"))).withColumn("defLon", udfRound(df("@lon")))
+    val rows = roundedDF.map(row => ((row.getDouble(0) ,row.getDouble(1)),1)).reduceByKey((a, b) => a + b)
 
     val maxIntensity = rows.takeOrdered(1)(Ordering[Int].reverse.on(_._2)).head
 
